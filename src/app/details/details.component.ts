46633a9b1bc3d499bb04db69696d88b5543f76a7
Genessis Skarlet Castillo <skarletcastillo@fisapay.com>
2024-12-17 16:05:11 -0500

    vista de detalles card

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../components/housing.service';
+import { Housinglocation } from '../components/housinglocation';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-details',
+  imports: [],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.scss'
+})
+export class DetailsComponent {
+  
+
+}
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Housinglocation } from '../components/housinglocation';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.scss'
 })
